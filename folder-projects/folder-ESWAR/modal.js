feff8299d650e6a68e1babcfe9b941502cf7a3d9
kelvinrjm <rjmkelvin@gmail.com>
2026-01-11 21:57:30 +0530

    Delete folder-projects/folder-ESWAR directory

--- folder-projects/folder-ESWAR/modal.js
+++
@@ -1,8 +0,0 @@
-function openModal(img,title){
-  document.getElementById("previewImg").src = img;
-  document.getElementById("previewTitle").innerText = title;
-  document.getElementById("imgModal").style.display = "flex";
-}
-function closeModal(){
-  document.getElementById("imgModal").style.display = "none";
-}
